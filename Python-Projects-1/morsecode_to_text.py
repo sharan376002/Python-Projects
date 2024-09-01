67a71c5d9a8328bd522f09735fe53a728882b7fb
sharan376002 <sharang.developer@gmail.com>
2024-09-02 00:00:45 +0530

    text to mosecode

---
+++ Python-Projects-1/morsecode_to_text.py
@@ -0,0 +1,43 @@
+symbols = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": ".-",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+}
+
+while True:
+    ask = input("Enter the name to translate : ")
+
+    length = len(ask)
+
+    output = ""
+
+    for i in range(length):
+        if ask[i] in symbols.keys():
+            output = output  + "  " +  symbols.get(ask[i])
+
+
+
+    print(output)
