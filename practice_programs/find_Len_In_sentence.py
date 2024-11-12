224807f406b6fe79f4e30adabf3cd1b5898eb1d1
Sharan.G <143382836+sharan376002@users.noreply.github.com>
2024-11-13 00:25:34 +0530

    Add files via upload

---
+++ practice_programs/find_Len_In_sentence.py
@@ -0,0 +1,9 @@
+senc = "the wisdom justice  temperence courage security figma bugbountry"
+
+words = senc.split()
+word_len = []
+for word in words:
+    word_len.append(len(word))
+    print(word, end=" ")
+print()
+print(word_len)   
