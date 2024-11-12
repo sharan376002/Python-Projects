be02c40c8d9cb6df2e8027986fe655b2b101a1f7
Sharan.G <143382836+sharan376002@users.noreply.github.com>
2024-11-13 00:26:33 +0530

    Add files via upload

---
+++ practice_programs/Subtraction_matrix_operation.py
@@ -0,0 +1,51 @@
+print("Matrix Subtraction operation")
+row =int(input("Enter the number of rows  : "))
+col =int(input("Enter the number of col  : "))
+
+matrix_A = [[0,0,0],
+            [0,0,0],
+            [0,0,0,]]
+print("Enter the values of matrix A : ")
+for i in range(row+1):
+    for j in range(col+1):
+        matrix_A[i][j] = input(f"Enter the value of a{i}{j} ")
+print("\n")
+print("Matrix A")
+for a in matrix_A:
+    print(a)
+
+matrix_B = [[0,0,0],
+            [0,0,0],
+            [0,0,0,]]
+
+print("Enter the values of matrix B : ")
+for i in range(row+1):
+    for j in range(col+1):
+        matrix_B[i][j] = input(f"Enter the value of b{i}{j} ")
+
+print("\n")
+print("Matrix B")
+for b in matrix_B:
+    print(b)
+
+
+
+
+
+sub = [[0,0,0],
+         [0,0,0],
+        [0,0,0,]]
+
+for i in range(len(matrix_A)):
+    for j in range(len(matrix_B)):
+        sub[i][j] = matrix_A[i][j] - matrix_B[i][j]
+    
+
+print("\n")
+
+print("Added Matrix")
+for s in sub:
+
+    print(s)
+
+
