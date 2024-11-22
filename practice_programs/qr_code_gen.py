5816e6bb3eab255b1bdd99097fc46ced39e50d21
Sharan.G <143382836+sharan376002@users.noreply.github.com>
2024-11-22 23:47:40 +0530

    Add files via upload

---
+++ practice_programs/qr_code_gen.py
@@ -0,0 +1,47 @@
+# install all the libaries needed 
+# create a function that collects a text and converts it to a Qr code
+# save the qrcode as a image 
+# run the function
+
+import qrcode # type: ignore
+
+def generate_qrcode(text):
+
+  qr = qrcode.QRCode( 
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 4,
+   )
+ 
+  qr.add_data(text)
+  qr.make(fit=True)
+  img = qr.make_image(fill_color="Black", back_color="White")
+  img.save("giri01.png")
+
+
+
+generate_qrcode("PODA SORI thalaya,PODA SORI thalaya,PODA SORI thalaya PODA SORI thalaya PODA SORI thalaya PODA SORI thalaya PODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalayaPODA SORI thalaya ")
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
